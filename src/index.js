13634faf1f152f979a8fc25b2230b25219195f86
vijay3778 <dididivya999@gmail.com>
2021-09-05 18:04:52 +0530

    corrected authorization

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin","*")
     res.header(
         "Access-Control-Allow-Headers",
-        "Origin, X-Request-With, Content-Type, Accept, Autherization"
+        "Origin, X-Request-With, Content-Type, Accept, Authorization"
     )
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
     next()
